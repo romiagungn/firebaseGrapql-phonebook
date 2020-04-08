b08dcaf996d258bf82139eb12892c68e328a7a8c
romiagungn <romian1394@gmail.com>
2020-04-08 20:00:13 +0700

    api crud worked done

--- api/app.js
+++ api/app.js
@@ -2,6 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const firebase = require("firebase");
+
+const config = {
+    apiKey: "AIzaSyCt1O_lE1SQd89IRPPVVzm7-OeidvG1Ipk",
+    authDomain: "phonebook-go.firebaseapp.com",
+    databaseURL: "https://phonebook-go.firebaseio.com",
+    projectId: "phonebook-go",
+    storageBucket: "phonebook-go.appspot.com",
+    messagingSenderId: "619960918463"
+};
+firebase.initializeApp(config);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require("firebase");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  const phoneBook = firebase.database().ref("/Phonebook/");
+  //Attach an asynchronous callback to read the data
+  phoneBook.on("value", function(snapshot) {
+    console.log(snapshot.val());
+    res.json(snapshot.val());
+    phoneBook.off("value");
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+    res.send("The read failed: " + errorObject.code);
+  });
+});
+
+//Create new instance
+router.post('/', function (req, res) {
+  const userName = req.body.username;
+  const name = req.body.name;
+  const nomor = req.body.nomor;
+
+  const referencePath = '/Phonebook/'+userName+'/';
+  const userReference = firebase.database().ref(referencePath);
+  userReference.set({Name: name, Nomor: nomor}, function(error) {
+    if (error) {
+      res.send("Data could not be saved." + error);
+    } else {
+      res.send("Data saved successfully.");
+    }
+  });
+});
+
+//Update existing instance
+router.put('/:username', function (req, res) {
+  const userName = req.params.username;
+  const name = req.body.name;
+  const nomor = req.body.nomor;
+
+  var referencePath = '/Phonebook/'+userName+'/';
+  var userReference = firebase.database().ref(referencePath);
+  userReference.update({Name: name, Nomor: nomor}, function(error) {
+    if (error) {
+      res.send("Data could not be updated." + error);
+    } else {
+      res.send("Data updated successfully.");
+    }
+  });
+});
+
+//Delete an instance
+router.delete('/:username', function (req, res) {
+  var userName = req.params.username;
+  var referencePath = '/Phonebook/'+userName+'/';
+  var userReference = firebase.database().ref(referencePath);
+  userReference.remove((error)=>{
+    if (error) {
+      res.send("Data could not be deleted." + error);
+    } else {
+      res.send("Data deleted successfully.");
+    }
+  })
 });
 
 module.exports = router;
